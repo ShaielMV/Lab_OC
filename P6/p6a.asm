13f44db29eb10f1d5cfe91eed18f7f967d298aed
ShaielMV <villela2219166@uabc.edu.mx>
2025-10-22 17:07:44 +0000

    commit

--- P6/p6a.asm
+++ P6/p6a.asm
@@ -1,21 +1,22 @@
 %include "../LIB/pc_iox.inc"
 
-section	.text
 
-	global _start       ;must be declared for using gcc
 
-_start:                     ;tell linker entry point
-    mov eax, 0x2219166
+	global _start      
+
+_start: 
+	mov eax, 0x2219166
 	mov ebx, 0x5C4B2A60
-    add dword ebx
-	call pHex_dw	; muestra en pantalla 64
+    add eax, ebx
 
-	mov al,10	; cambio de linea
+	call pHex_dw	
+
+	mov al,10	
 	call putchar
 
 	
 
 
-	mov eax, 1	;system call number (sys_exit) -- fin del programa
-	int 0x80        ;call kernel
+	mov eax, 1
+	int 0x80        
 
