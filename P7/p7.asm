86147f5e87cce767283c72f36681536abf15ee21
ShaielMV <villela2219166@uabc.edu.mx>
2025-10-30 00:41:42 +0000

    commit

--- P7/p7.asm
+++ P7/p7.asm
@@ -28,11 +28,16 @@ _start:
     call putchar 
 	call putchar
 
-    mov esi, 0x20D685F3     ;inciso c
-    mov eax, 0x40042021          
+    mov eax, 0x20D685F3     ;inciso c
+    MOV esi, eax
+    call pBin_dw
+     mov al, 10
+    call putchar
+
+    mov eax, 0x40042021
     xor esi, eax
     mov eax, esi
-    call pHex_dw
+    call pBin_dw
 
     mov al, 10
     call putchar 
@@ -42,21 +47,30 @@ _start:
 
 
     mov ch, 0xA7    ;inciso e
+    mov al, ch
+    call pBin_b
+     mov al, 10
+    call putchar
+
     mov ah, 0x48
     or ch, ah
     mov al, ch
-    call pHex_b
+   call pBin_b
 
     mov al, 10
     call putchar 
 	call putchar
 
     mov bp, 0x67DA  ;inciso f
-    mov ax, 0xBBAD
+    mov ax, bp
+    call pBin_w
+    mov al, 10
+    call putchar 
 
+    mov ax, 0xBBAD
     and bp, ax
     mov ax, bp
-    call pHex_w
+    call pBin_w
 
     mov al, 10
     call putchar 
@@ -88,7 +102,7 @@ _start:
     call putchar 
 	call putchar
 
-    pop esi  ;inciso j
+    pop esi      ;inciso j
     mov eax, esi
     call pHex_dw
 
@@ -96,7 +110,16 @@ _start:
     call putchar 
 	call putchar
 
-    
+    mov eax, esi     ;inciso k
+    shl eax, 1                
+    shl esi, 3                
+    add eax, esi              
+    call pHex_dw
+
+    mov al, 10
+    call putchar 
+	call putchar
+
 
 	mov eax, 1
 	int 0x80        
