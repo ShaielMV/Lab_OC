4bac56adfa7536ac59d5ca9bfaf58980aa04ea15
ShaielMV <villela2219166@uabc.edu.mx>
2025-10-30 18:19:30 +0000

    commit

--- P8/p8.asm
+++ P8/p8.asm
@@ -1,20 +1,21 @@
 %include "../LIB/pc_iox.inc"
 	
-    
-
 	section	.text
-    extern getche
+
 
 	global _start  
     
 
 	section .data
+     msgnoLetra	db  'El caracter ingresado no es una letra',0xa,0
     msgMenorM	db  'La letra ingreada es menor a m',0xa,0 
     msgMayorM	db  'La letra ingreada es mayor a m',0xa,0 
-    
+    msgLetra	db  'El caracter ingresado es una letra',0xa,0 
+    msgNum	db  'El caracter ingresado es un numero',0xa,0 
+    asterisco dw '*',0
 	
 _start: 
-	call getche  ;inciso a
+	call getche     ;inciso a
     cmp al, 'a'
     jb noLetra
     cmp al, 'z'
@@ -29,21 +30,56 @@ _start:
     call puts
    
     noLetra:
-	jmp sig
+    mov edx, msgnoLetra
+    call puts
+	jmp sigb
 
     menorM:
     mov edx, msgMenorM
     call puts
-    jmp sig
+    jmp sigb
 
 
-    sig:        ;inciso b
+    sigb:        ;inciso b
     call getche  
     cmp al, 'a'
-    
+    jb num
     cmp al, 'z'
+    ja num
+
+    mov edx, msgLetra
+    call puts
+    jmp sigc
+
+    num:
+    cmp al, '0'
+    jae esnum
+    cmp al, '9'
+    jbe esnum
+    jmp sigc
+
+    esnum:
+    mov edx, msgNum
+    call puts
+
+    sigc:       ;inciso c
+    mov cx, 3
     
-    
+ print: 
+    mov ax, cx
+    mov edx, asterisco
+    cant:
+        call puts
+        dec ax
+        cmp ax, 0
+        ja cant
+        mov al, 10
+        call putchar
+ loop print
+
+     mov [edx]           ;inciso d
+
+         
 
 	mov eax, 1
 	int 0x80        
