5a442df1946742b956e1f935ca39ca777827e645
ShaielMV <villela2219166@uabc.edu.mx>
2025-11-12 17:13:49 +0000

    commit

--- P9/p9.asm
+++ P9/p9.asm
@@ -1,118 +1,148 @@
 %include "../LIB/pc_iox.inc"
-n equ 3	
+n equ 3
 
 	section	.text
 
+    extern pHex_b
 	global _start  
     
 	section .data
     captura1 db 'Ingrese el primer vector',0
     captura2 db 'Ingrese el segundo vector',0
     sumav db 'La suma de los 2 vectores es =',0
-    arreglo db '0123456789',0
-    arreglo2 db '0123456789',0
+    escalarResultados db 'El producto escalar entre los 2 vectores es =',0
+    arreglo db "1234567890",0
+    arreglo2 db "1234567890",0
  
     
 _start: 
-	
     mov edx, captura1
     call puts
-    call c_linea
-    call capturar
+    call saltoLinea
+    mov ebx, arreglo
+    mov ecx, n
+    call capturarVector
 
     mov edx, captura2
     call puts
-    mov dl, 1
-    call c_linea
-    call capturar
+    call saltoLinea
+    mov ebx, arreglo2
+    call capturarVector
+
+    mov edx, escalarResultados
+    call puts
+    call productoEscalar
 
     mov edx, sumav
     call puts
-    call c_linea
-    call suma
-
-    mov ecx, n
-    mov ebx, arreglo
-    despl:
-        mov  al, [ebx]
-        call pHex_b
-        call c_linea
-        inc ebx
-    loop despl
-
- 
-    mov eax, 1
-    int 0x80
-
-    capturar:
     mov ebx, arreglo
     mov edx, arreglo2
-    mov ecx, n
-
-    cmp dl, 1
-    jz v2
-
-    ciclo:
-        call getch
-        r:
-        cmp al,'0'
-        jb r
-        cmp al,'9'
-        ja r
-
-        sub al,'0'
-        mov byte [ebx], al
-        inc ebx
-    loop ciclo
-    ret
-
-    v2:
-        call getch
-        t:
-        cmp al,'0'
-        jb t
-        cmp al,'9'
-        ja t
-        sub al, '0'
-        mov byte [edx], al
-        inc edx
-    loop v2
-    ret
-    
-
-
-    desplegar:
+    call sumaVectores
     mov ebx, arreglo
-    mov ecx, n
-    ciclo2:
-        mov al, byte[ebx]
+    
+    call desplegarVector
+
+
+   
+mov eax, 1
+	int 0x80 
+
+capturarVector:
+    push ecx
+    push ebx
+    push eax
+
+    repetir:
+        call getch
+        cmp al, '0'
+        jb repetir
+        cmp al, '9'
+        ja repetir
         call putchar
+        sub al, '0'
+        mov [ebx],al
         inc ebx
-    loop ciclo2
+    loop repetir
+    pop eax
+    pop ebx
+    pop ecx
+    call saltoLinea
     ret
 
-    suma:
-        mov ecx, n
-        mov ebx, arreglo
-        mov edx, arreglo2
-       
-        ciclo3:
-          mov al, byte[ebx]
-          add al, byte[edx]
-          mov byte[ebx], al
-          inc ebx
-          inc edx
-                  
-        loop ciclo3
-    ret
-
-
-
-    c_linea:
+    desplegarVector:
+        push ecx
+        push ebx
         push eax
-        mov al,10
+
+        cmp ecx,10
+        ja end
+        cmp ecx, 10
+        je end
+        mostrar:
+            call saltoLinea
+            mov al,[ebx]
+            call pHex_b
+            inc ebx
+        loop mostrar
+        pop eax
+        pop ebx
+        pop ecx
+    end:
+    call saltoLinea
+    ret
+
+    sumaVectores:
+        push ebx
+        push edx
+        push ecx
+        push eax
+
+        sumar:
+            mov al, [ebx]
+            add al, [edx]
+            mov [ebx], al
+            inc ebx
+            inc edx
+        loop sumar
+    pop eax
+    pop ecx
+    pop edx
+    pop ebx
+    ret
+
+    productoEscalar:
+        push ecx
+        push edi
+        push esi
+        push eax
+        push ebx
+        mov edx, 0
+        mov esi, arreglo
+        mov edi, arreglo2
+        sums:
+            mov bl, [esi]
+            mov al, [edi]
+            mul bl
+            add dx, ax
+            inc edi
+            inc esi
+        loop sums
+    mov ax, dx
+    call pHex_w
+    call saltoLinea
+    pop ebx
+    pop eax
+    pop esi
+    pop edi
+    pop ecx
+ret
+
+
+
+    
+    saltoLinea:
+        push eax
+        mov al, 10
         call putchar
         pop eax
     ret
-mov eax, 1
-	int 0x80 
